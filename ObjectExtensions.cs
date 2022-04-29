3e8e0e2f93c413feac6e08cf0abbfc427da94d5f
Dan Roberts <dangger36@gmail.com>
2022-04-29 15:24:28 +0100

    Initial commit

---
+++ ObjectExtensions.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+using Microsoft.Diagnostics.Runtime;
+
+namespace ClrMdTest
+{
+    public static class ObjectExtensions
+    {
+        public delegate bool CanHandle(Type type);
+        public delegate IEnumerable<string>? Render(object value);
+
+        public static void Dump(
+            this object value,
+            string title,
+            ImmutableArray<(CanHandle CanHandle, Render GetLines)>? typeHandlers = null,
+            Action<string>? writeTo = null)
+        {
+            Console.WriteLine($"= {title} ========----------------------");
+            Console.WriteLine();
+            foreach (var line in Dump(value, parents: Enumerable.Empty<object>(), typeHandlers ?? DefaultTypeHandlers) ?? Enumerable.Empty<string>())
+            {
+                (writeTo ?? Console.WriteLine)(line);
+            }
+        }
+
+        public static readonly ImmutableArray<(CanHandle CanHandle, Render GetLines)> DefaultTypeHandlers =
+            ImmutableArray<(CanHandle, Render)>.Empty
+                .Add((
+                    type => typeof(Type).IsAssignableFrom(type),
+                    value => new[] { ((Type)value).FullName! }))
+                .Add((
+                    type => typeof(ClrRuntime).IsAssignableFrom(type),
+                    value => null)) // There isn't a convenient Name-like property to display and we don't want to dig into all of the modules, threads, etc..
+                .Add((
+                    type => typeof(ClrAppDomain).IsAssignableFrom(type) || typeof(ClrModule).IsAssignableFrom(type),
+                    value => new[] { value.ToString()! }))
+                .Add((
+                    type => typeof(ClrType).IsAssignableFrom(type),
+                    value => new[] { ((ClrType)value).Name! }))
+                .Add((
+                    type => typeof(IEnumerable<ClrStackFrame>).IsAssignableFrom(type),
+                    value => RenderStackTrace((IEnumerable<ClrStackFrame>)value)));
+
+        private static IEnumerable<string>? Dump(object value, IEnumerable<object> parents, ImmutableArray<(CanHandle CanHandle, Render GetLines)> typeHandlers)
+        {
+            if (value is null)
+            {
+                return new[] { $"{GetIndentation(parents.Count())}null" };
+            }
+
+            if (parents.Contains(value))
+            {
+                return new[] { $"{GetIndentation(parents.Count())}*Circular Reference*" };
+            }
+
+            var type = value.GetType();
+            foreach (var (canHandle, getLines) in typeHandlers)
+            {
+                if (canHandle(type))
+                {
+                    return getLines(value)?.Select(line => $"{GetIndentation(parents.Count())}{line}");
+                }
+            }
+            
+            // Apply primitive handling (unless there was a custom renderer in the typeHandlers list for the current type)
+            if (type.IsPrimitive || type.IsEnum || type == typeof(DateTime) || type == typeof(DateTimeOffset) || type == typeof(string))
+            {
+                var displayValue = type == typeof(int) || type == typeof(uint) || type == typeof(short) || type == typeof(ushort) || type == typeof(long) || type == typeof(ulong)
+                    ? type.GetMethod("ToString", new[] { typeof(string) })!.Invoke(value, new[] { "n0" })
+                    : value.ToString();
+
+                return new[] { $"{GetIndentation(parents.Count())}{displayValue}" };
+            }
+
+            var getEnumerator = type.GetInterfaces()
+                .Select(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)
+                    ? i.GetMethod("GetEnumerator")
+                    : null)
+                .FirstOrDefault(t => t is not null);
+            if (getEnumerator is not null)
+            {
+                var enumerator = (IEnumerator)getEnumerator.Invoke(value, null)!;
+                var items = new List<IEnumerable<string>>();
+                while (enumerator.MoveNext())
+                {
+                    var lines = Dump(enumerator.Current, parents.Append(value), typeHandlers);
+                    if (lines is not null)
+                    {
+                        if (IsSingleLine(lines))
+                        {
+                            items.Add(new[] { $"{GetIndentation(parents.Count())}{lines.First().Trim()}" });
+                        }
+                        else
+                        {
+                            if (items.Any())
+                            {
+                                lines = lines.Prepend("");
+                            }
+                            items.Add(lines);
+                        }
+                    }
+                }
+                if (!items.Any())
+                {
+                    items.Add(new[] { $"{GetIndentation(parents.Count())}[]" });
+                }
+                return items.SelectMany(lines => lines);
+            }
+
+            var fieldsAndProperties = type
+                .GetFields()
+                .Select(field => (field.Name, Value: field.GetValue(value)))
+                .Concat(
+                    type
+                        .GetProperties()
+                        .Where(property => property.GetIndexParameters().Length == 0)
+                        .Select(property => (property.Name, Value: property.GetValue(value))));
+            if (!fieldsAndProperties.Any())
+            {
+                return new[] { GetIndentation(parents.Count()) + "{}" };
+            }
+            else
+            {
+                var items = new List<IEnumerable<string>>();
+                foreach (var (name, memberValue) in fieldsAndProperties)
+                {
+                    var lines = Dump(memberValue, parents.Append(value), typeHandlers);
+                    if (lines is not null)
+                    {
+                        if (IsSingleLine(lines))
+                        {
+                            items.Add(new[] { $"{GetIndentation(parents.Count())}{name}: {lines.First().Trim()}" });
+                        }
+                        else
+                        {
+                            items.Add(new[] { $"{GetIndentation(parents.Count())}{name}:" }.Concat(lines));
+                        }
+                    }
+                }
+                return items.SelectMany(lines => lines);
+            }
+        }
+
+        private static bool IsSingleLine(IEnumerable<string> lines) => lines.Take(2).Count() == 1;
+
+        private static string GetIndentation(int indentationLevel) => new(' ', indentationLevel * 2);
+
+        private static IEnumerable<string> RenderStackTrace(IEnumerable<ClrStackFrame> stackTrace)
+        {
+            if (!stackTrace.Any())
+            {
+                return new[] { "None available" };
+            }
+
+            return stackTrace
+                .Append(null) // Add a null entry to the end to ensure any leftover NumberOfUnknownFramesQueued value isn't discarded
+                .Aggregate(
+                    seed: new { NumberOfUnknownFramesQueued = 0, Lines = Enumerable.Empty<string>() },
+                    func: (acc, frame) =>
+                    {
+                        if (frame is not null && frame.Method is null && frame.FrameName is null)
+                        {
+                            return new { NumberOfUnknownFramesQueued = acc.NumberOfUnknownFramesQueued + 1, acc.Lines };
+                        }
+                        var lines = acc.Lines;
+                        if (acc.NumberOfUnknownFramesQueued > 0)
+                        {
+                            lines = lines.Append((acc.NumberOfUnknownFramesQueued > 1 ? $"{acc.NumberOfUnknownFramesQueued}x " : "") + "Unknown");
+                        }
+                        if (frame is not null)
+                        {
+                            lines = lines.Append($"{frame.Method?.Signature ?? frame.FrameName ?? "Unknown"} [{frame.Kind}]");
+                        }
+                        return new { NumberOfUnknownFramesQueued = 0, Lines = lines };
+                    })
+                .Lines;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,179 @@
+﻿using System.Diagnostics;
+using ClrMdTest;
+using Microsoft.Diagnostics.Runtime;
+
+var path = args.FirstOrDefault();
+if (string.IsNullOrWhiteSpace(path))
+{
+	Console.WriteLine("No file specified in the command line arguments");
+	Environment.Exit(1);
+}
+if (!File.Exists(path))
+{
+	Console.WriteLine("Specified file path does not exist: " + path);
+	Environment.Exit(1);
+}
+
+var timer = Stopwatch.StartNew();
+
+Console.WriteLine($"[{timer.Elapsed.TotalSeconds:0.00}s] Loading: {path}");
+using (var target = DataTarget.LoadDump(path))
+{
+	Console.WriteLine($"[{timer.Elapsed.TotalSeconds:0.00}s] Loaded");
+	Console.WriteLine();
+
+	var runtime = target.ClrVersions.First().CreateRuntime();
+
+	var retrievals = new Action[]
+    {
+		() => PrintRuntimeInfo(runtime),
+		() => PrintMemoryRegionInfo(runtime),
+		() => PrintHeapAnalysis(runtime),
+		() => PrintThreadAnalysis(runtime, onlyIfGotException: false),
+		() => PrintThreadAnalysis(runtime, onlyIfGotException: true)
+	};
+
+	foreach (var retrieval in retrievals)
+    {
+		retrieval();
+		Console.WriteLine($"^ Done after {timer.Elapsed.TotalSeconds:0.00}s");
+		Console.WriteLine();
+	}
+}
+
+Console.WriteLine($"[{timer.Elapsed.TotalSeconds:0.00}s] Done! Press [Enter] to terminate..");
+Console.ReadLine();
+
+static void PrintRuntimeInfo(ClrRuntime runtime) =>
+	new
+	{
+		Version = runtime.ClrInfo.Version.ToString(),
+		ServerGC = runtime.Heap.IsServer ? "Yes" : "No",
+		runtime.DataTarget!.DataReader.Architecture,
+		TargetPlatform = runtime.DataTarget!.DataReader.TargetPlatform.ToString(),
+		Bitness = runtime.DataTarget!.DataReader.PointerSize == 8 ? "x64" : "x86",
+		AppDomainCount = runtime.AppDomains.Length,
+		runtime.AppDomains,
+		Modules = runtime.EnumerateModules(),
+		Threads = runtime.Threads.Length,
+		Heaps = runtime.Heap.LogicalHeapCount
+	}
+	.Dump("Runtime Info");
+
+static void PrintMemoryRegionInfo(ClrRuntime runtime)
+{
+	var segmentGroups = runtime.Heap.Segments
+		.GroupBy(segment => segment.LogicalHeap)
+		.OrderBy(group => group.Key)
+		.Select(group => new { Heap = group.Key, Size = group.Sum(segment => (uint)segment.Length) })
+		.ToArray();
+
+	const string title = "Memory Region Information";
+	if (segmentGroups.Length == 0)
+	{
+		Console.WriteLine($"{title}: None available");
+	}
+	else
+    {
+		segmentGroups.Dump(title);
+    }
+}
+
+static void PrintHeapAnalysis(ClrRuntime runtime)
+{
+	var heap = runtime.Heap;
+
+	ulong totalStringObjectSize = 0, stringObjectCounter = 0;
+	var stringCounts = new Dictionary<string, ulong>();
+	var typeCounts = new Dictionary<string, (ulong Bytes, ulong Count)>();
+	foreach (var obj in heap.EnumerateObjects())
+	{
+		var type = obj.Type;
+		if (type is null)
+		{
+			continue;
+		}
+
+		var size = obj.Size;
+		var name = type.Name!;
+		if (typeCounts.TryGetValue(name, out var t))
+		{
+			typeCounts[name] = (t.Bytes + size, t.Count + 1);
+		}
+		else
+		{
+			typeCounts[name] = (size, 1);
+		}
+
+		if (type.IsString)
+		{
+			stringObjectCounter++;
+			var text = obj.AsString(maxLength: int.MaxValue);
+			if (text is not null)
+			{
+				totalStringObjectSize += size;
+				stringCounts[text] = stringCounts.TryGetValue(text, out var count) ? count + 1 : 1;
+            }
+		}
+	}
+
+	typeCounts
+		.OrderByDescending(w => w.Value.Bytes)
+		.Select(w => new { w.Value.Bytes, w.Value.Count, Type = w.Key })
+		.Take(100)
+		.Dump("Top 100 Types (By Size)");
+
+	Console.WriteLine();
+	typeCounts
+		.OrderByDescending(w => w.Value.Count)
+		.Select(w => new { w.Value.Count, w.Value.Bytes, Type = w.Key })
+		.Take(100)
+		.Dump("Top 100 Types (By Count)");
+
+	Console.WriteLine();
+	stringCounts
+		.OrderByDescending(w => w.Value)
+		.Select(w => new { Count = w.Value, Value = w.Key })
+		.Take(100)
+		.Dump("Top 100 Most Common Strings");
+
+	Console.WriteLine();
+	stringCounts
+		.OrderByDescending(w => w.Key.Length)
+		.Select(w => new { Count = w.Value, Size = w.Key.Length.ToString("n0"), Value = w.Key.Length > 10000 ? w.Key[..10000] + "..." : w.Key })
+		.Take(100)
+		.Dump("1000 Largest Strings");
+
+	Console.WriteLine($"Overall {stringObjectCounter:N0} \"System.String\" objects take up {totalStringObjectSize:N0} bytes ({(totalStringObjectSize / 1024.0 / 1024.0):N2} MB)");
+}
+
+static void PrintThreadAnalysis(ClrRuntime runtime, bool onlyIfGotException) =>
+	runtime.Threads
+		.Where(t => t.EnumerateStackTrace().Any())
+		.Where(t => (t.CurrentException is not null) || !onlyIfGotException)
+		.Select(t => new
+		{
+			t.IsAlive,
+			t.OSThreadId,
+			t.ManagedThreadId,
+			t.GCMode,
+			t.IsFinalizer,
+			t.LockCount,
+			Address = t.Address.ToString("x"),
+			Details = new
+			{
+				t.IsAbortRequested,
+				t.IsAborted,
+				t.IsGCSuspendPending,
+				t.IsUserSuspended,
+				t.IsDebugSuspended,
+				t.IsBackground,
+				t.IsUnstarted,
+				t.IsCoInitialized,
+				t.IsSTA,
+				t.IsMTA
+			},
+			t.CurrentException,
+			StackTrace = t.EnumerateStackTrace()
+		})
+		.Dump($"{(onlyIfGotException ? "" : "All ")}Managed Threads{(onlyIfGotException ? " with Exceptions" : "")}");
